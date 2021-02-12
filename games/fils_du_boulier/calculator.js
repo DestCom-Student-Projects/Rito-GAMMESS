cda3308ec98df1bef9ff514bea7eed73397dcfd9
Stanley.js <76979070+Stan-Jms@users.noreply.github.com>
2021-02-12 21:17:49 +0100

    Add files via upload

---
+++ games/fils_du_boulier/calculator.js
@@ -0,0 +1,16 @@
+var acc=0;
+
+function prendre_val(valeur)
+{
+    acc = document.calc.affi.value+= valeur; //accumule les valeur (string) du doc dans un acc
+}
+
+function rez()
+{
+    document.calc.affi.value=eval(acc); // eval étant risqué je l'utilise en étant conscient de ses risques, il convertit les valeurs string en maths
+}
+
+function delete_aff()
+{
+    document.calc.affi.value="";
+}
